b3dfb5dd5166bdaa1e82aee62c41f735d8221b3b
PROCENTX <romkagrigorev@mail.ru>
2022-07-06 22:01:06 +0300

    в bitap добавлен русский алфавит, добавлен сам bitap

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var CRC32 = require("crc-32");
 const fs = require('fs');
 const readline = require('readline-sync');
+var bitap = require('bitap')
 
 let rawdata = fs.readFileSync('data.json');
 var sentence = JSON.parse(rawdata);
@@ -10,31 +10,24 @@ var sentence = JSON.parse(rawdata);
 // console.log(str.indexOf(str_i));
 
 
-//поиск предложений по строке
-const str = readline.question("Введите строку: ");
 
 //перевод в нижний регистр
 //let str_low = str.toLowerCase();
 //console.log(str_low);
 
-//console.time('Firstway');
-for (var i = 0; i < 100000; i++){
-  let str_sentence = sentence.words[i];
-  if (str_sentence.indexOf(str) != -1){
-    console.log(str_sentence);
-  }
-}
-//console.timeEnd('Firstway');
+//поиск предложений по строке
+// const str = readline.question("Введите строку: ");
 
+//console.time('Firstway'); //начало таймера
+  // for (var i = 0; i < 100000; i++){
+  //   let str_sentence = sentence.words[i];
+  //   if (str_sentence.indexOf(str) != -1){
+  //     console.log(str_sentence);
+  //   }
+  // }
+//console.timeEnd('Firstway'); //конец таймера
 
-// const data = 'спасибо';
-// const data1 = 'спасиба'
-// const hash = CRC32.str(data);
-// const hash1 = CRC32.str(data1);
-// const index1 = Math.abs(hash) % 1000;
-// const index2 = Math.abs(hash1) % 1000;
-//console.log(index1);
-//console.log(index2);
+console.log(bitap('космический', 'комич', 2));
 
 
 
