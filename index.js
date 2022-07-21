f8a6f48c28671f6c8c3816e1d012d6e5c38e5d02
PROCENTX <romkagrigorev@mail.ru>
2022-07-21 18:07:45 +0300

    добалвены нужные библиотеки

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
-const fs = require('fs');
-const readline = require('readline-sync');
-var bitap = require('bitap')
+import fs from 'fs';
+import readline from 'readline-sync'
+import bitap from 'bitap';
+import NGrams from 'ngrams-search';
 
+//импорт датафрейма
+import DataFrame from 'dataframe-js';
+
+//чтение данных
 let rawdata = fs.readFileSync('data.json');
 var sentence = JSON.parse(rawdata);
 
-// const str_i = readline.question("Введите строку: ");
-// let str = 'Норси-транс компания состоящая из людей';
-// console.log(str.indexOf(str_i));
+const df = new DataFrame(sentence);
 
-
-
-//поиск предложений по строке
-const str = readline.question("Введите строку: ").toLowerCase();//поиск только по нижнему регистру
-//console.log(str);
-
-
-console.time('Firstway'); //начало таймера
-  for (var i = 0; i < 100000; i++){
-    let str_sentence = sentence.words[i].toLowerCase();
-    //console.log(str_sentence);
-    if (bitap(str_sentence, str, 1).length != 0) {
-      //console.log(str_sentence);
-    }
-  }
-console.timeEnd('Firstway'); //конец таймера
-
-// let str_c = 'воздевать'
-// console.log(bitap(str_c, str, 2));
-
-
-
-
-
-//console.log(sentence.words); //вывод всего JSON
+console.log(df);
