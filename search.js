c99b53a3f18ab05a9585c66da6fa6656e1442721
PROCENTX <romkagrigorev@mail.ru>
2022-07-25 17:09:57 +0300

    сделан поиск

--- search.js
+++ search.js
@@ -75,13 +75,26 @@ function search(str){
         }
       }
     }
+  } else {
+    let words_set = map_word.keys(str)
+    for( var runner_words of words_set){
+        var n_gram_index = n.compare(str, runner_words)
+        if (n_gram_index > 0.62){
+          let words_set = map_word.get(runner_words)
+          for (var runner_words_index of words_set){
+            let sentence_set = map_word_sentence.get(runner_words_index)
+            for(var search_sentence of sentence_set){
+              finish_array_sentence.push(search_sentence)
+            }
+          }
+      }
+    }
   }
 }
 //самовольный
 search(str);
 // console.log(finish_array_sentence);
 for (var a of finish_array_sentence){
-  // console.log(a);
   console.log(sentence.words[a]);
 }
 console.timeEnd("Firstway")
