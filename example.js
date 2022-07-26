243320cb4eb574b7be298b92cf83cec0ea87b798
PROCENTX <romkagrigorev@mail.ru>
2022-07-26 22:42:06 +0300

    исправлено название в examle.js

--- example.js
+++ example.js
@@ -3,6 +3,6 @@ import {index_create} from './index.js'
 import {index_search} from './search.js'
 
 let sentence = x
-const index = index_create(sentence);
-const example_search = index_search(index[0], index[1], 'савольный');
+const indexing_map = index_create(sentence);
+const example_search = index_search(indexing_map[0], indexing_map[1], 'савольный');
 example_search.forEach(e => console.log(e));
